82bdd7834cfe970df370ec4d55c91c4499254118
immerzen <kasimpson@gmail.com>
2025-03-19 16:09:10 -0500

    updating latest changes

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,12 @@
+import pygame
 from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        pass
+        super().__init__(x, y, radius)
 
-    def draw(self, ):
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (self.x, self.y), 2)
+
+    def update(self, dt):
+        super().velocity * dt
